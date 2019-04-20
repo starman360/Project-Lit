803e1491f4734b103e1053112fb76202ad979221
ceb3745 <ceb3745@g.rit.edu>
2019-04-20 14:12:49 -0400

    got the google api call working

--- webcampage/src/App.js
+++ webcampage/src/App.js
@@ -1,33 +1,125 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import WebcamComponent from './components/WebcamComponent';
-
-
 import './App.css';
+import Camera from 'react-camera';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <WebcamComponent />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+export default class App extends Component {
 
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.takePicture = this.takePicture.bind(this);
+        //setInterval(this.takePicture, 3000);
+        this.state = {
+            url : "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAZyRx-Nv-pimpzKJlrse4ik-qblG9Mgxc"
+        }
+    }
+
+    takePicture() {
+        this.camera.capture()
+            .then(blob => {
+                this.img.src = URL.createObjectURL(blob);
+                this.img.onload = () => { URL.revokeObjectURL(this.src); };
+                this.getBase64Image(this.img.src).then(blob => {
+                    this.sendRequest(blob);
+                });
+
+
+
+            })
+    }
+
+    getBase64Image(url) {
+
+        var promise = new Promise(function(resolve, reject) {
+
+            var img = new Image();
+            // To prevent: "Uncaught SecurityError: Failed to execute 'toDataURL' on 'HTMLCanvasElement': Tainted canvases may not be exported."
+            img.crossOrigin = "Anonymous";
+            img.onload = function() {
+                var canvas = document.createElement("canvas");
+                canvas.width = img.width;
+                canvas.height = img.height;
+                var ctx = canvas.getContext("2d");
+                ctx.drawImage(img, 0, 0);
+                var dataURL = canvas.toDataURL("image/png");
+                resolve(dataURL.replace(/^data:image\/(png|jpg|jpeg|pdf);base64,/, ""));
+            };
+            img.src = url;
+        });
+
+        return promise;
+    };
+
+
+    sendRequest(url) {
+        var request = {
+            "requests": [
+                {
+                    "features": [
+                        {
+                            "type": "TEXT_DETECTION"
+                        }
+                    ],
+                    "image": {
+                        "content" : url
+                    }
+                }
+            ]
+        };
+
+        const {labelAnnotations} =  fetch(this.state.url, {
+            method: 'POST',
+            body: JSON.stringify(request)
+        });
+
+        console.log(labelAnnotations);
+    }
+
+    render() {
+        return (
+            <div style={style.container}>
+                <Camera
+                    style={style.preview}
+                    ref={(cam) => {
+                        this.camera = cam;
+                    }}
+                >
+                    <div style={style.captureContainer} onClick={this.takePicture}>
+                        <div style={style.captureButton} />
+                    </div>
+                </Camera>
+                <img id = "imageid"
+                    style={style.captureImage}
+                    ref={(img) => {
+                        this.img = img;
+                    }}
+                />
+            </div>
+        );
+    }
 }
 
-export default App;
+const style = {
+    preview: {
+        position: 'relative',
+    },
+    captureContainer: {
+        display: 'flex',
+        position: 'absolute',
+        justifyContent: 'center',
+        zIndex: 1,
+        bottom: 0,
+        width: '100%'
+    },
+    captureButton: {
+        backgroundColor: '#fff',
+        borderRadius: '50%',
+        height: 56,
+        width: 56,
+        color: '#000',
+        margin: 20
+    },
+    captureImage: {
+        width: '100%',
+    }
+};
+
