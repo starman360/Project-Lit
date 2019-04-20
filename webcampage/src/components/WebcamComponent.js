ce5b0bae0bb719113174d1af93b04a1e59c5a3ef
ceb3745 <ceb3745@g.rit.edu>
2019-04-20 12:24:39 -0400

    added stuff to have webcam feed

---
+++ webcampage/src/components/WebcamComponent.js
@@ -0,0 +1,32 @@
+import {Component} from "react";
+import React from "react";
+import App from "../App";
+
+export class WebcamComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.videoTag = React.createRef()
+        this.state = {
+            url : "https://vision.googleapis.com/v1/images:annotate?key=AIzaSyAZyRx-Nv-pimpzKJlrse4ik-qblG9Mgxc"
+        }
+    }
+
+    componentDidMount() {
+        // getting access to webcam
+        navigator.mediaDevices
+            .getUserMedia({video: true})
+            .then(stream => this.videoTag.current.srcObject = stream)
+            .catch(console.log);
+    }
+
+    render() {
+        return (<video id={this.props.id}
+                      ref={this.videoTag}
+                      width={this.props.width}
+                      height={this.props.height}
+                      autoPlay
+                      title={this.props.title} />);
+    }
+}
+
+export default WebcamComponent;
