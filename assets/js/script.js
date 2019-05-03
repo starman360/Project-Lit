7acb22e188b0c081eb3a4465d3f5da5863ca0b6f
Anmol Modur <anmol.modur@gmail.com>
2019-05-03 14:18:15 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,29 +27,34 @@ $(document).ready(function () {
         // console.log(audio.currentTime);
     });
 
+    var availableRows = [0, 1, 2, 3, 4, 5, 6, 7];
+
     $('button#play').click(function () {
         audio.play();
         $(this).fadeOut(1000);
         var id = setInterval(frame, 10);
         var pos = 30000;
-        var availableRows = [0, 2, 3, 4, 5, 6, 7];
         function frame() {
             if (pos == 4156000) {
                 clearInterval(id);
             } else {
                 if (typeof (json['' + pos]) != 'undefined') {
-                    var index = availableRows[Math.floor(Math.random() * availableRows.length)];
+                    var i = Math.floor(Math.random() * availableRows.length);
+                    console.log('pulled ' + i);
+                    var index = availableRows.splice(i,1);
+                    console.log(availableRows + '\t\t' + index);
                     setTimeout(function () {
-                        availableRows.append(index);
-                    }, 2500);
-                    $('#lanes').append('<p id="t' + pos + '" class="p'+index+'">' + json['' + pos] + '</p>');
+                        console.log('pushing ' + index);
+                        availableRows.push(index);
+                    }, 1500);
+                    $('#lanes').append('<p id="t' + pos + '" class="p' + index + '">' + json['' + pos] + '</p>');
                     $('#t' + pos).animate({
                         left: "-20vw"
-                    }, 5000,"linear", function () {
+                    }, 5000, "linear", function () {
                         $(this).remove();
                     });
                 }
-            }
+            } 
             pos += 10;
         };
     });
